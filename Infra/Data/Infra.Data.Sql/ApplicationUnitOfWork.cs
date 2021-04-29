7d22c2427e35753fc9a709eff15b01af4d6f20a6
MrComic <mohammad.farahani70@gmail.com>
2021-04-29 19:02:50 +0430

    افزودن UnitOfWorrk

---
+++ Infra/Data/Infra.Data.Sql/ApplicationUnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrlShortner.Framework.Domain.Data;
+
+namespace UrlShortner.Infra.Data.SqlServer
+{
+    public class ApplicationUnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public ApplicationUnitOfWork(ApplicationDbContext advertismentDbContext)
+        {
+            this.applicationDbContext = advertismentDbContext;
+        }
+        public int Commit()
+        {
+            var entityForSave = GetEntityForSave();
+            int result = applicationDbContext.SaveChanges();
+            return result;
+        }
+
+        private List<EntityEntry> GetEntityForSave()
+        {
+            return applicationDbContext.ChangeTracker
+              .Entries()
+              .Where(x => x.State == EntityState.Modified || x.State == EntityState.Added || x.State == EntityState.Deleted)
+              .ToList();
+        }
+    }
+}
---
+++ Infra/Data/Infra.Data.Sql/ShortenedUrls/UrlShortnetRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrlShortner.Core.Domain.ShortenedUrls.Data;
+
+namespace UrlShortner.Infra.Data.SqlServer.ShortenedUrls
+{
+    public class UrlShortnetRepository : IUrlShortnerRepository, IDisposable
+    {
+        public void Dispose()
+        {
+            this.applicationDbContext.Dispose();
+        }
+
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public UrlShortnetRepository(ApplicationDbContext advertismentDbContext)
+        {
+            this.applicationDbContext = advertismentDbContext;
+        }
+    }
+}
